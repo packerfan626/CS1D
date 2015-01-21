6a3795e0af8b2e3d171401624ec5cf4bb4ba5975
packerfan626 <dori_mouawad@hotmail.com>
2015-01-20 21:10:00 -0800

    Changes

---
+++ Assignment1/header.h
@@ -0,0 +1,20 @@
+/*********************************************************
+ *  AUTHOR       : Dori J. Mouawad
+ *  Assignment#1 : Linked Lists
+ *  CLASS	     : CS1D
+ *  SECTION      : TTH: 5:30-9:20pm
+ *  Due Date     : 1/27/2015
+ *********************************************************/
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+
+#endif /* HEADER_H_ */
--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,2 +1,36 @@
-//blah
-//josh
+/*********************************************************
+ *  AUTHOR       : Dori J. Mouawad
+ *  Assignment#1 : Linked Lists
+ *  CLASS	     : CS1D
+ *  SECTION      : TTH: 5:30-9:20pm
+ *  Due Date     : 1/27/2015
+ *********************************************************/
+
+#include "header.h"
+
+int main()
+{
+	string string1 = "dad";
+	string string2 = "radar";
+	string string3 = "Otto";
+	string string4 = "mom";
+	string string5 = "tigers";
+	string string6 = "A man, a plan, a canal, Panama";
+	string string7 = "Isn’t this class cool?";
+	string string8 = "Was it a car or a cat I saw?";
+	string string9 = "Sit on a potato pan, Otis";
+	string string10 = "Saddleback is a great place to learn";
+
+
+	return 0;
+}
+
+bool isPalindrome (string astring)
+{
+
+	if(isalpha(letter))
+	{
+
+	}
+
+}
