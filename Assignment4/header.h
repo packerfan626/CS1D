029304f84db35e93c8991ff5ef8fc37f8025a7a2
packerfan626 <dori_mouawad@hotmail.com>
2015-02-15 16:57:43 -0800

    Added Assignment 4

---
+++ Assignment4/header.h
@@ -0,0 +1,19 @@
+/*********************************************************
+ *  AUTHOR       : Dori J. Mouawad
+ *  Assignment#4 : Stacks
+ *  CLASS	     : CS1D
+ *  SECTION      : TTH: 5:30-9:20pm
+ *  Due Date     : 2/17/2015
+ *********************************************************/
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+
+#endif /* HEADER_H_ */
---
+++ Assignment4/main.cpp
@@ -0,0 +1,23 @@
+/*********************************************************
+ *  AUTHOR       : Dori J. Mouawad
+ *  Assignment#4 : Stacks
+ *  CLASS	     : CS1D
+ *  SECTION      : TTH: 5:30-9:20pm
+ *  Due Date     : 2/17/2015
+ *********************************************************/
+
+#include "header.h"
+
+int main()
+{
+	//PRINT HEADING
+	cout << "*****************************************\n";
+	cout << "   Programmed by: Dori J. Mouawad\n";
+	cout << "   Student ID   : 356008\n";
+	cout << "   CS1D         : TTH- 5p-9:20\n";
+	cout << "   Assignment#4 : n";
+	cout << "*****************************************\n";
+
+
+	return 0;
+}
