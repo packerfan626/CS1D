749e15ecf72fef4cb9d2f6c05baec336af60c4d1
packerfan626 <dori_mouawad@hotmail.com>
2015-02-26 17:00:44 -0800

    Assignment 6 More Starters

---
+++ Assignment6/header.h
@@ -0,0 +1,19 @@
+/*********************************************************
+ *  AUTHOR       : Dori J. Mouawad
+ *  Assignment#6 : Priority Queue
+ *  CLASS	     : CS1D
+ *  SECTION      : TTH: 5:30-9:20pm
+ *  Due Date     : 3/2/2015
+ *********************************************************/
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+
+#endif /* HEADER_H_ */
---
+++ Assignment6/main.cpp
@@ -0,0 +1,22 @@
+/*********************************************************
+ *  AUTHOR       : Dori J. Mouawad
+ *  Assignment#6 : Priority Queue
+ *  CLASS	     : CS1D
+ *  SECTION      : TTH: 5:30-9:20pm
+ *  Due Date     : 3/2/2015
+ *********************************************************/
+
+#include "header.h"
+
+int main()
+{
+	//PRINT HEADING
+	cout << "*****************************************\n";
+	cout << "   Programmed by: Dori J. Mouawad\n";
+	cout << "   Student ID   : 356008\n";
+	cout << "   CS1D         : TTH- 5p-9:20\n";
+	cout << "   Assignment#6 : Priority Queue \n";
+	cout << "*****************************************\n";
+
+
+}
